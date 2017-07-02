45746e2b4943ab2812ce4dce909a37bbda0f9f09
Evan Gaustad <evan.gaustad@gmail.com>
2017-07-02 16:33:03 -0500

    scales dates to x hours before current time

---
+++ adjust_dates.py
@@ -0,0 +1,59 @@
+#! /usr/bin/env python
+import json
+from datetime import datetime, timedelta
+import os
+import argparse
+import sys
+
+def get_log_files(path):
+    files = []
+    for myfile in os.listdir(path):
+        if myfile.endswith(".log"):
+            match = os.path.join(path, myfile)
+            files.append(match)
+
+    return files
+
+
+if __name__=='__main__':
+    parser = argparse.ArgumentParser(description='Fixes dates on json format bro logs relative to some time period')
+
+    parser.add_argument('-d', '--logs_directory', help='Path where logs will be modified.')
+    parser.add_argument('-h', '--hours', help='Number hours from curent time to adjust logs to.')
+    args = parser.parse_args()
+
+    # Use Case: Analyze samples on disk.
+    if args.logs_directory:
+        if not os.path.exists(args.input):
+            print "\nSupplied log_directory path did not exist.  Exiting.\n"
+            parser.print_help()
+            sys.exit(1)
+
+    try:
+        hours = int(args.hours)
+    except:
+        print "\nMust supply a valid number of hours relative to current time to start logs from."
+        parser.print_help()
+        sys.exit(1)
+
+    path = args.logs_directory
+    files = get_log_files(path)
+    d = datetime.today() - timedelta(hours=hours)
+    new_start_time = int(d.strftime('%s'))
+
+    for log_file in files:
+        print log_file
+        with open(log_file) as mylog:
+            logs = mylog.readlines()
+    
+        orig_start_time = json.loads(logs[0]).get('ts', 0)
+        os.remove(log_file)
+        
+        with open(log_file, 'wb') as write_log:
+            for log in logs:
+                log_dictionary = json.loads(log)
+                event_time = log_dictionary.get('ts', -1)
+                if event_time > -1:
+                    log_dictionary['ts'] = float(event_time) - orig_start_time + new_start_time
+                write_log.write(json.dumps(log_dictionary) + '\n')
+
